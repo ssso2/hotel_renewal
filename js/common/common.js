48c75cb41f8214198ecd636ed6dcba3eeb882ea6
손주혜 <gagaclub@naver.com>
2024-09-12 00:29:18 +0900

    guide 작업 중

--- js/common/common.js
+++ js/common/common.js
@@ -39,3 +39,24 @@ for(let i = 0; i < tabs.length; i++){
 //     }
 // });
 
+
+
+// DEPTH3 스와이퍼 슬라이드 갤러리
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+});
