fd6acbca2ecf70c24929e4881031c92b8663da68
손주혜 <gagaclub@naver.com>
2024-09-16 05:07:57 +0900

    메인 header 와 서브 header 분리

--- js/common/header.js
+++ js/common/header.js
@@ -1,1 +1,40 @@
 
+const header = document.querySelector("header");
+const gnb = document.querySelector(".gnb");
+const gnbBg = document.querySelector(".gnbbg");
+const gnb1Depth = document.querySelectorAll(".gnb > li");
+
+gnb.addEventListener("mouseover",function(){
+    gnbBg.classList.add("on");
+});
+gnb.addEventListener("mouseleave",function(){
+    gnbBg.classList.remove("on");
+});
+for(let i = 0; i < gnb1Depth.length; i++){
+    gnb1Depth[i].addEventListener("mouseleave",function(){
+        gnb1Depth[i].classList.remove("on");
+    });
+    gnb1Depth[i].addEventListener("mouseover",function(){
+        gnb1Depth[i].classList.add("on");
+    });
+}
+
+// 과거의 스크롤바 위치값
+let lastScrollTop = 0;
+
+window.addEventListener("scroll",function(){
+    // 스크롤바의 위치값
+    let scTop = window.scrollY;
+
+    //스크롤바 내리면 헤더는 사라지고 스크롤바 올리면 헤더 나타남
+    if(scTop == 0){
+        header.classList.add("active");
+    }
+    if(scTop > lastScrollTop) {
+        header.classList.remove("active");
+    }
+    else{
+        header.classList.add("active");
+    }
+    lastScrollTop = scTop;
+});
--- js/main/main.js
+++ js/main/main.js
@@ -3,7 +3,26 @@ AOS.init();
 
 // 메인페이지 Header
 const mainHeader = document.querySelector(".main-header header");
+const gnb = document.querySelector(".gnb");
+const gnbBg = document.querySelector(".gnbbg");
+const gnb1Depth = document.querySelectorAll(".gnb > li");
 const mainCont1 = document.querySelector("main > .cont1");
+
+
+gnb.addEventListener("mouseover",function(){
+    gnbBg.classList.add("on");
+});
+gnb.addEventListener("mouseleave",function(){
+    gnbBg.classList.remove("on");
+});
+for(let i = 0; i < gnb1Depth.length; i++){
+    gnb1Depth[i].addEventListener("mouseleave",function(){
+        gnb1Depth[i].classList.remove("on");
+    });
+    gnb1Depth[i].addEventListener("mouseover",function(){
+        gnb1Depth[i].classList.add("on");
+    });
+}
 
 
 // 과거의 스크롤바 위치값
