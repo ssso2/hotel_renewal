985eb1090fd1fa81c587864e2a177540ec776ec8
손주혜 <gagaclub@naver.com>
2024-09-19 16:11:21 +0900

    d-day-wrap 수정

--- js/sub/sub01_01_01.js
+++ js/sub/sub01_01_01.js
@@ -1,15 +1,21 @@
 $(function () {
     // 스페셜오퍼 상세페이지
     // 지금바로예약하기 하단에 붙이기
-    let scTop = 0;
-    let dDayPosition = $(".d-day-wrap").offset().top;
-    let dDayHeight = $(".d-day-wrap").outerHeight();
+
     $(window).on("scroll", function () {
-        scTop = window.scrollY;
-        if (scTop >= dDayPosition + dDayHeight) {
+        let scTop = window.scrollY;
+        let dDayPosition = $(".d-day-wrap").offset().top;
+        let dDayHeight = $(".d-day-wrap").outerHeight();
+
+        // 
+        if (scTop >= dDayPosition + dDayHeight - $(window).height()) {
             $(".d-day-wrap").addClass("on");
         }
+        else{
+            $(".d-day-wrap").removeClass("on");
+        }
+
     });
 
-    // console.log(dDayPosition + dDayHeight);
+    
 });
