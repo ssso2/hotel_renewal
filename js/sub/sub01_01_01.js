6f631788896cf5e378c16c0b437ec81653a4b67b
sohee <01okok@naver.com>
2024-09-20 16:13:05 +0900

    Merge branch 'master' into sub/sohee
    # Conflicts:
    #	css/login.css
    #	scss/sub00.scss
    #	scss/sub01_01_01.scss

--- js/sub/sub01_01_01.js
+++ js/sub/sub01_01_01.js
@@ -1,30 +1,20 @@
 $(function () {
     // 스페셜오퍼 상세페이지
     // 지금바로예약하기 하단에 붙이기
-
     $(window).on("scroll", function () {
         let scTop = window.scrollY;
         let dDayPosition = $(".d-day-wrap").offset().top;
         let dDayHeight = $(".d-day-wrap").outerHeight();
         let scrollY = dDayPosition + dDayHeight - $(window).height();
 
-        // 
+        //
         if (scTop > scrollY) {
             $(".d-day-wrap").addClass("on");
-        }
-        else if(scTop <= 1260){
+        } else if (scTop <= 1260) {
             $(".d-day-wrap").removeClass("on");
         }
 
-
-        console.log("scTop : ",scTop);
-        console.log("scrollY : ",scrollY);
-        
-
+        console.log("scTop : ", scTop);
+        console.log("scrollY : ", scrollY);
     });
-
 });
-
-
-
-
