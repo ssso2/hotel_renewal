c2911e597db332b05bfde098402f4bd1d0cab440
손주혜 <gagaclub@naver.com>
2024-09-23 13:38:29 +0900

    예약 작업

---
+++ js/sub/reservation.js
@@ -0,0 +1,23 @@
+const reservationSearchBtn = document.querySelector(".reservation-search-btn");
+const noSelect = document.querySelector(".no-select");
+const searchResults = document.querySelector(".search-results-wrap");
+const searchResultsTab = document.querySelectorAll(".search-results-wrap .tab > li");
+const searchResultsTabCont = document.querySelectorAll(".search-results-wrap .tab-cont");
+
+// 예약 페이지 검색 버튼 놀렀을 때 아래 검색 내용 보이게 하기
+reservationSearchBtn.addEventListener("click",function(){
+    noSelect.classList.remove("on");
+    searchResults.classList.add("on");
+});
+
+// 예약페이지 검색 결과 탭 이동
+for(let i = 0; i < searchResultsTab.length; i++){
+    searchResultsTab[i].addEventListener("click",function(){
+        for(let j = 0; j < searchResultsTab.length; j++){
+            searchResultsTab[j].classList.remove("on");
+            searchResultsTabCont[j].classList.remove("on");
+        }
+        searchResultsTab[i].classList.add("on");
+        searchResultsTabCont[i].classList.add("on");
+    });
+}
