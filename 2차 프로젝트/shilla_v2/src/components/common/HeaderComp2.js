b3dcd57175980dc6c7566576ff0e841a7a89eed6
손주혜 <gagaclub@naver.com>
2024-12-09 11:29:04 +0900

    관리자 로그인 작업

--- 2차 프로젝트/shilla_v2/src/components/common/HeaderComp2.js
+++ 2차 프로젝트/shilla_v2/src/components/common/HeaderComp2.js
@@ -11,35 +11,42 @@ const HeaderComp2 = ({user}) => {
         navigate('/login');
     }
 
+
+    const LoginGroup = () => {
+        if(user && user.grade == 1){
+            return  <div className="btn-wrap admin">
+                        <Link to="/admin" className="user-name-btn" title="관리자페이지로 이동">
+                            <i className="fa-regular fa-user"></i>
+                            <p>관리자<span>님</span></p>
+                        </Link>
+                        <button className="logout-btn" onClick={logout}>로그아웃
+                            <i className="fa-solid fa-arrow-right-from-bracket"></i>
+                        </button>
+                    </div>
+        }
+        else if(user){
+            return  <div className="btn-wrap on login">
+                        <Link to="/mypage" className="user-name-btn" title="마이페이지로 이동">
+                            <i className="fa-regular fa-user"></i>
+                            <p>{user.name}<span>님</span></p>
+                        </Link>
+                        <button className="logout-btn" onClick={logout}>로그아웃
+                            <i className="fa-solid fa-arrow-right-from-bracket"></i>
+                        </button>
+                    </div>
+        }
+        else {
+            return  <div className="btn-wrap on">
+                        <Link to="/join" className="join-btn"><i className="fa-solid fa-user-plus"></i>회원가입</Link>
+                        <Link to="/login" className="login-btn">로그인<i className="fa-solid fa-arrow-right-to-bracket"></i> </Link>
+                    </div>
+        }
+    }
+
     
     return (
         <>
-            {
-                (user) ? 
-                <div className="btn-wrap on login">
-                    <Link to="/mypage-new" className="user-name-btn" title="마이페이지로 이동">
-                        <i className="fa-regular fa-user"></i>
-                        <p>{user.name}<span>님</span></p>
-                    </Link>
-                    <button className="logout-btn" onClick={logout}>로그아웃
-                        <i className="fa-solid fa-arrow-right-from-bracket"></i>
-                    </button>
-                </div>
-                : <div className="btn-wrap on">
-                    <Link to="/join" className="join-btn"><i className="fa-solid fa-user-plus"></i>회원가입</Link>
-                    <Link to="/login" className="login-btn">로그인<i className="fa-solid fa-arrow-right-to-bracket"></i> </Link>
-                </div>
-            }
-            {/* <!-- 관리자 로그인시 --> */}
-            <div className="btn-wrap admin">
-                <Link to="/admin" className="user-name-btn" title="관리자페이지로 이동">
-                    <i className="fa-regular fa-user"></i>
-                    <p>관리자<span>님</span></p>
-                </Link>
-                <Link to="/" className="logout-btn">로그아웃
-                    <i className="fa-solid fa-arrow-right-from-bracket"></i>
-                </Link>
-            </div>
+            <LoginGroup/>
         </>
     );
 };
