ddbf5c2becb176678c0fc061f6a6f56a28208b9c
poolname <lkm02124@gmail.com>
2024-12-17 12:05:02 +0900

    1217 커밋

--- 2차 프로젝트/shilla_v2/src/components/room/Exec_ExecBusinessDeluxe.js
+++ 2차 프로젝트/shilla_v2/src/components/room/Exec_ExecBusinessDeluxe.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Exec_ExecBusinessDeluxe(props) {
     return (
         <div>
-            <h2>Exec_ExecBusinessDeluxe Page</h2>
+            {/* <h2>Exec_ExecBusinessDeluxe Page</h2> */}
         </div>
     );
 }
--- 2차 프로젝트/shilla_v2/src/components/room/Tab.js
+++ 2차 프로젝트/shilla_v2/src/components/room/Tab.js
@@ -16,16 +16,16 @@ const Tab = () => {
     return (
         <ul className="tab">
             <li className={`tab1 ${location.pathname === '/standard' ? 'on' : ''}`}>
-                <Link to="/standard">디럭스</Link>
+                <Link to="/standard/deluxe">디럭스</Link>
             </li>
             <li className={`tab2 ${location.pathname === '/businessDeluxe' ? 'on' : ''}`}>
-                <Link to="/businessDeluxe">비즈니스 디럭스</Link>
+                <Link to="/standard/businessDeluxe">비즈니스 디럭스</Link>
             </li>
             <li className={`tab3 ${location.pathname === '/barrierFreeDeluxe' ? 'on' : ''}`}>
-                <Link to="/barrierFreeDeluxe">배리어프리 비즈니스 디럭스</Link>
+                <Link to="/standard/barrierFreeDeluxe">배리어프리 비즈니스 디럭스</Link>
             </li>
             <li className={`tab4 ${location.pathname === '/grandCornerDeluxe' ? 'on' : ''}`}>
-                <Link to="/grandCornerDeluxe">그랜드 코너 디럭스</Link>
+                <Link to="/standard/grandCornerDeluxe">그랜드 코너 디럭스</Link>
             </li>
         </ul>
     )
