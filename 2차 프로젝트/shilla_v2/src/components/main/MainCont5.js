abeaf1cc9615268c39bb30593ccde2188a2a117f
손주혜 <gagaclub@naver.com>
2024-12-17 12:03:07 +0900

    main cont5 팝업 오류 수정

--- 2차 프로젝트/shilla_v2/src/components/main/MainCont5.js
+++ 2차 프로젝트/shilla_v2/src/components/main/MainCont5.js
@@ -3,13 +3,15 @@ import '../../scss/reset.css'
 import '../../scss/common.scss'
 import '../../scss/main.scss'
 import { useEffect, useState } from 'react'
+import $ from 'jquery';
 import MainCont5Popup from './MainCont5Popup'
 
 const MainCont5 = () => {
+
     const MainCont5Data = [
         {
             delay : "0",
-            link : "#self",
+            link : "javascrip:void();",
             title : "신라호텔 지점 찾기 팝업 열림",
             target : '',
             dataLybtn : "pop-hotel-box",
@@ -22,7 +24,7 @@ const MainCont5 = () => {
             link : "https://www.shillahotels.com/membership/offers/shillaS/memShillaS.do",
             title : "신라S 멤버십 안내 페이지 새창 열기",
             target : '_blank',
-            dataLybtn : "",
+            dataLybtn : null,
             iClass : "fa-brands fa-sketch",
             h3Text : '신라에스 멤버십',
             pText :"서울 &middot; 제주신라호텔에서 자유롭게 이용 가능한 통합 유료 멤버십, 신라 S"
@@ -32,40 +34,48 @@ const MainCont5 = () => {
             link : "https://www.shillahotels.com/membership/offers/benefits/memBenefitsIndex.do",
             title : "신라리워즈 회원특전 페이지 새창 열기",
             target : '_blank',
-            dataLybtn : "",
+            dataLybtn : null,
             iClass : "fa-solid fa-gifts",
             h3Text : '신라 리워즈',
             pText :"신라 호텔 계열 및 제휴 호텔에서 포인트 혜택 및 특전을 제공하는 통합 멤버십 서비스"
         },
     ]
 
-    let [MainCont5,MainCont5Set] = useState(MainCont5Data)
 
 
-    useEffect(()=>{
+    useEffect(() => {
         $(function () {
-            // 레이어 팝업
+            // 레이어 팝업 닫기
             $(".lypop_close").on("click", function () {
                 $(".lypop").hide();
             });
-        
+    
+            // data-lybtn을 가진 요소를 클릭했을 때
             $("[data-lybtn]").each(function () {
                 var lypop = $(this).attr("data-lybtn");
-                $(this).click(function () {
-                    $(".lypop").hide();
-                    $("[data-lyOpen =" + lypop + "]")
-                        .show()
-                        .focus();
-                });
-                $("[data-lyclose]").click(function () {
-                    var lypopClose = $(this).attr("data-lyclose");
-                    $("[data-lyOpen =" + lypop + "]").hide();
-                    $("[data-lybtn =" + lypopClose + "]").focus();
-                });
+    
+                // lypop 값이 존재하는 경우에만 동작하도록 수정
+                if (lypop) {
+                    $(this).click(function () {
+                        $(".lypop").hide();
+                        // data-lyOpen 속성 값을 가진 요소를 찾고 보이게 함
+                        $("[data-lyOpen='" + lypop + "']").show().focus();
+                    });
+                } else {
+                    console.error("data-lybtn 속성이 비어있습니다.");
+                }
             });
-
+    
+            // data-lyclose을 가진 요소 클릭 시 처리
+            $("[data-lyclose]").click(function () {
+                var lypopClose = $(this).attr("data-lyclose");
+                $("[data-lyOpen='" + lypopClose + "']").hide();
+                $("[data-lybtn='" + lypopClose + "']").focus();
+            });
         });
-    },[])
+    }, []);
+
+    
 
 
 
@@ -80,7 +90,7 @@ const MainCont5 = () => {
                     </div>
                     <ul className="benefit">
                         {
-                            MainCont5.map((item,index)=>{
+                            MainCont5Data.map((item,index)=>{
                                 return  <li key={index} className="list" data-aos="fade-up" data-aos-delay={item.delay}>
                                             <Link to={item.link} title={item.title}  target={item.target} data-lybtn={item.dataLybtn}>
                                                 <i className={item.iClass}></i>
