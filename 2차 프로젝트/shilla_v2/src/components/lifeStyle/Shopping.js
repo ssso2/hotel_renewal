d427965ec9a10ccf93a8fc672e8fa6d8a86e7c02
bmnbjo <bmnbjo@gmail.com>
2024-12-18 17:44:54 +0900

    웨딩,다이닝,라이프 메인 컴포넌트화

--- 2차 프로젝트/shilla_v2/src/components/lifeStyle/Shopping.js
+++ 2차 프로젝트/shilla_v2/src/components/lifeStyle/Shopping.js
@@ -5,7 +5,6 @@ import SubTitle from './SubTitle';
 import Gallery2 from './Gallery2';
 import Introduction from './Introduction';
 import Location from './Location';
-// import Desc from './Desc';
 import Footer from '../common/Footer';
 
 import "../../scss/common.scss"
