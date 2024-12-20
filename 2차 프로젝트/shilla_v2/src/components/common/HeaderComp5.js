df8ffe9297915b8e3b00028945eb178bcfdff361
손주혜 <gagaclub@naver.com>
2024-12-20 10:30:20 +0900

    header 선생님과 작업

--- 2차 프로젝트/shilla_v2/src/components/common/HeaderComp5.js
+++ 2차 프로젝트/shilla_v2/src/components/common/HeaderComp5.js
@@ -1,29 +1,38 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import '../../scss/reset.css'
 import '../../scss/common.scss'
 import '../../scss/header.scss'
 import '../../scss/main.scss'
 
-const HeaderComp5 = ({gnbMenu,winPage,setWinPage}) => {
+//const HeaderComp5 = ({gnbMenu,winPage,setWinPage, aaa}) => {
+    const HeaderComp5 = ({gnbMenu}) => {    
     
-    const handlePageChange = (newPage) => {
-        if (winPage !== newPage) {
-            setWinPage(newPage);
-        }
-    };
+    function bbb( wp){
+        console.log("bbb : ",  wp)
+        //setWinPage(wp)
+        //Navigate(uuu)
+    }
+       
 
     return (
         <ul className="m_gnb">
             {
                 gnbMenu.map((item,index)=>{
                     return  <li key={index} >
+                               
                                 <Link to='#self'>{item.title}</Link>
-                                <ul className="m_sub">
+                                <ul className="m_sub" >
                                     {
+                                        
                                         item.gnbMenu.map((depth2,idx)=>{
-                                            return <li key={idx}><Link to={depth2.link} onClick={() => handlePageChange(winPage === 0 ? 1 : 0)}>{depth2.text}</Link></li>
+                                            return <li key={idx}><Link to={depth2.link} >{depth2.text}</Link></li>  
+                                            // return <div key={idx}><Link onClick={()=>bbb(depth2.text)} to={depth2.link} >{depth2.text}</Link></div>  
+                                            // return <div key={idx}><Link onClick={()=>setWinPage(depth2.text)} to={depth2.link} >{depth2.text}</Link></div>
+                                            //   return <div key={idx}><Link to={depth2.link} onClick={()=>bbb( depth2.link, depth2.text)}>{depth2.text}</Link></div>
+                                            //return <div onClick={bbb} key={idx}> wefwefwef {depth2.text}</div> 
                                         })
+                                        
                                     }
                                 </ul>
                             </li>
