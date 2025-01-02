46c5d6eb6e6314d8008140687c0ce7bbaf4479fb
suyn04 <63181309+suyn04@users.noreply.github.com>
2025-01-02 11:33:16 +0900

    01/02 modules upload

--- 3차 프로젝트/shilla_v2/src/components/join/JoinCont2.js
+++ 3차 프로젝트/shilla_v2/src/components/join/JoinCont2.js
@@ -1,33 +1,36 @@
-import React, { useState } from "react";
-import JoinDateSelector from "./JoinDateSelector";
-import JoinName from "./JoinName";
-import JoinNameEng from "./JoinNameEng";
-import JoinPw from "./JoinPw";
-import JoinPhone from "./JoinPhone";
-import JoinEmail from "./JoinEmail";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import JoinDateSelector from './JoinDateSelector';
+import JoinName from './JoinName';
+import JoinNameEng from './JoinNameEng';
+import JoinPw from './JoinPw';
+import JoinPhone from './JoinPhone';
+import JoinEmail from './JoinEmail';
 import '../../scss/myinfo.scss';
 
 const bkURL = process.env.REACT_APP_BACK_URL;
 
 const JoinCont2 = () => {
-    const [id, setId] = useState('');  // 아이디 상태
-    const [valid, setValid] = useState(false);  // 유효성 상태
-    const [errorMessage, setErrorMessage] = useState('');  // 에러 메시지 상태
-    const [idAvailable, setIdAvailable] = useState(null);  // 중복 여부 상태
-    const [isPopupVisible, setIsPopupVisible] = useState(false);  // 팝업 표시 여부
-    const [popupMessage, setPopupMessage] = useState('');  // 팝업 메시지
-    const [nameEng, setNameEng] = useState('');  // 영어 이름 상태 (JoinNameEng 컴포넌트와 연동)
-    const [pw, setPw] = useState('');  // 패스워드 상태
-    const [nameKor, setNameKor] = useState('');  // 이름 상태
-    const [email, setEmail] = useState('');  // 이메일 상태
-    const [phone, setPhone] = useState('');  // 전화번호 상태
-    const [birth, setBirth] = useState('');  // 생년월일 상태
+    const [id, setId] = useState(''); // 아이디 상태
+    const [valid, setValid] = useState(false); // 유효성 상태
+    const [errorMessage, setErrorMessage] = useState(''); // 에러 메시지 상태
+    const [idAvailable, setIdAvailable] = useState(null); // 중복 여부 상태
+    const [isPopupVisible, setIsPopupVisible] = useState(false); // 팝업 표시 여부
+    const [popupMessage, setPopupMessage] = useState(''); // 팝업 메시지
+    const [nameEng, setNameEng] = useState(''); // 영어 이름 상태 (JoinNameEng 컴포넌트와 연동)
+    const [pw, setPw] = useState(''); // 패스워드 상태
+    const [nameKor, setNameKor] = useState(''); // 이름 상태
+    const [email, setEmail] = useState(''); // 이메일 상태
+    const [phone, setPhone] = useState(''); // 전화번호 상태
+    const [birth, setBirth] = useState(''); // 생년월일 상태
 
     const [isEmailValid, setEmailValid] = useState(true);
     const [isNameValid, setNameValid] = useState(true);
     const [isNameEngValid, setNameEngValid] = useState(true);
     const [isPhoneValid, setPhoneValid] = useState(true);
     const [isPwValid, setPwValid] = useState(true);
+
+    const navigate = useNavigate();
 
     const idtype = /^[A-Za-z0-9]{6,}$/; // 아이디 유효성 정규표현식
 
@@ -71,7 +74,7 @@ const JoinCont2 = () => {
                 setPopupMessage('이미 사용 중인 아이디입니다.');
             }
         } catch (error) {
-            console.error("아이디 중복 확인 오류:", error);
+            console.error('아이디 중복 확인 오류:', error);
             setPopupMessage('아이디 중복 확인 오류');
         } finally {
             // 팝업을 보이게 설정
@@ -87,36 +90,38 @@ const JoinCont2 = () => {
     // 회원가입 폼 제출
     const handleSubmit = async (e) => {
         e.preventDefault(); // 폼 제출 기본 동작 방지
-    
+
         const formData = {
-            member_id: id,  // 아이디 상태 값
-            pw: pw,  // 패스워드 상태 값
-            name: nameKor,  // 이름 상태 값
-            name_eng: nameEng,  // 영어 이름 상태 값
-            email: email,  // 이메일 상태 값
-            phone: phone,  // 전화번호 상태 값
-            birth: birth,  // 생년월일 상태 값
+            member_id: id, // 아이디 상태 값
+            pw: pw, // 패스워드 상태 값
+            name: nameKor, // 이름 상태 값
+            name_eng: nameEng, // 영어 이름 상태 값
+            email: email, // 이메일 상태 값
+            phone: phone, // 전화번호 상태 값
+            birth: birth, // 생년월일 상태 값
         };
-    
+
         try {
             const response = await fetch(`${bkURL}/join/`, {
-                method: "POST",
+                method: 'POST',
                 headers: {
-                    "Content-Type": "application/json",
+                    'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(formData), // JSON 형식으로 보내기
             });
-    
+
             const data = await response.json();
-    
+
             if (data.ret) {
                 alert('회원가입 성공');
             } else {
                 alert('회원가입 실패');
             }
+
+            navigate('/Welcome');
         } catch (error) {
-            console.error("회원가입 오류:", error);
-            alert("회원가입 오류 발생");
+            console.error('회원가입 오류:', error);
+            alert('회원가입 오류 발생');
         }
     };
 
@@ -134,11 +139,13 @@ const JoinCont2 = () => {
                         {/* 영문 이름 입력 */}
                         <JoinNameEng valid={isNameEngValid} setValid={setNameEngValid} setNameEng={setNameEng} />
                         {/* 생년월일 입력 */}
-                        <JoinDateSelector valid={valid} setValid={setValid} setBirth={setBirth}/>
+                        <JoinDateSelector valid={valid} setValid={setValid} setBirth={setBirth} />
                         {/* 아이디 입력 */}
                         <div className="info-group">
                             <div className="input-container">
-                                <label htmlFor="pid">아이디 <font color="#F00">*</font></label>
+                                <label htmlFor="pid">
+                                    아이디 <font color="#F00">*</font>
+                                </label>
                                 <div className="input-wrap">
                                     <input
                                         name="member_id"
@@ -149,8 +156,12 @@ const JoinCont2 = () => {
                                         value={id}
                                         onChange={handleIdChange}
                                     />
-                                    <button type="button" id="id-chk" data-lybtn="pop-using" onClick={handleIdCheck}>중복확인</button>
-                                    <span className="error-message" id="id-error">{errorMessage}</span>
+                                    <button type="button" id="id-chk" data-lybtn="pop-using" onClick={handleIdCheck}>
+                                        중복확인
+                                    </button>
+                                    <span className="error-message" id="id-error">
+                                        {errorMessage}
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -159,11 +170,19 @@ const JoinCont2 = () => {
                         {/* 연락처 입력 */}
                         <JoinPhone valid={isPhoneValid} setValid={setPhoneValid} setPhone={setPhone} />
                         {/* 이메일 입력 */}
-                        <JoinEmail valid={isEmailValid} setValid={setEmailValid} setEmail={setEmail} errorMessage={errorMessage} setErrorMessage={setErrorMessage} />
+                        <JoinEmail
+                            valid={isEmailValid}
+                            setValid={setEmailValid}
+                            setEmail={setEmail}
+                            errorMessage={errorMessage}
+                            setErrorMessage={setErrorMessage}
+                        />
 
                         <div className="info-group">
                             <div className="input-container">
-                                <button type="submit" id="submit" disabled={!isFormValid}>가입하기</button>
+                                <button type="submit" id="submit" disabled={!isFormValid}>
+                                    가입하기
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -181,7 +200,9 @@ const JoinCont2 = () => {
                             <div className="lypop-ct">
                                 <p className="modal-txt">{popupMessage}</p>
                                 <div className="btn-wrap type5">
-                                    <button className="btn btn-04" onClick={closePopup}><span>확인</span></button>
+                                    <button className="btn btn-04" onClick={closePopup}>
+                                        <span>확인</span>
+                                    </button>
                                 </div>
                             </div>
                         </div>
